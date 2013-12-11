fb34e713922e6c1ed91e1af0579017f5284523a3
TareqM <tareqstudios@gmail.com>
2013-12-11 15:45:26 -0800

    created literal notation object and object constructor

---
+++ objects/object1.js
@@ -0,0 +1,15 @@
+// literal notation object
+var object1 = {
+    name: 'Jim',
+    address: '129 Broadway St'
+};
+
+// object constructor
+var object2 = new Object();
+object2.name = "Anthony";
+object2.kaza = "zaza";
+
+var object3 = {
+    what: "yes",
+    who: "maybe",
+};
